d0292c6e45d2879b57d626ba50159e33b4aa6219
Aakash <43797724+swapnil112@users.noreply.github.com>
2020-10-17 01:57:57 +0530

    sort_dict_by_key

---
+++ sort_dict_by_key.py
@@ -0,0 +1,15 @@
+def dictionairy():       
+ key_value ={}     
+ key_value[2] = 56       
+ key_value[1] = 2 
+ key_value[5] = 12 
+ key_value[4] = 24
+ key_value[6] = 18      
+ key_value[3] = 323 
+ print(sorted(key_value.items(), key = 
+             lambda kv:(kv[1], kv[0])))     
+   
+def main(): 
+    dictionairy() 
+if __name__=="__main__":       
+    main() 
